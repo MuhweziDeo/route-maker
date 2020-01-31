0822b32ab49866d38acbc25eb54c151777df4fe3
muhwezideo <aggrey256@gmail.com>
2020-01-31 19:22:19 +0300

    setup map view

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { Marker } from "react-native-maps";
+import * as Permissions from 'expo-permissions';
+
+import * as Location from 'expo-location';
+
 
 export default function App() {
+  const [location, setLocation] = useState({lat:37.78825, long: -122.4324})
+
+  useEffect(async() => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      await Permissions.askAsync(Permissions.LOCATION);
+    }
+    let loc = await Location.getCurrentPositionAsync({});
+    setLocation({lat: loc.coords.latitude, long: loc.coords.longitude})
+  },[location])
+
   return (
     <View style={styles.container}>
-      <Text>Loading</Text>
+      <Text>Map</Text>
+      <MapView 
+        region={{
+          latitude: location.lat,
+          longitude: location.long,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+        style={styles.mapStyle}
+        >
+        <Marker
+          coordinate={{
+            latitude: location.lat,
+            longitude: location.long
+          }}
+          pinColor="blue"
+          />
+        </MapView>
     </View>
   );
 }
@@ -12,9 +45,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    color: "white"
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
